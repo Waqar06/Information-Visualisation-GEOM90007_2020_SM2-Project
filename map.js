64eb858bf1151fe610f0e85e1877e3d7e509ecf8
papakhin <papakhin.mdy@gmail.com>
2020-10-15 19:17:33 +1100

    Update 15 Oct

--- map.js
+++ map.js
@@ -1,24 +1,38 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoicGFwYWtoaW4iLCJhIjoiY2tldHV6djFwMjg4MjJ5c2F1NWZpdnJ3ZSJ9.-vTJowNTOjlikU7GhVD3HA'; 
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2FuaXN1enphbWFuIiwiYSI6ImNrZXVuMnVvcTFjanMycHNhbjdxM2N6eGMifQ.KR21Chk0yGFmjK8d7Y0fHg'; 
 	var map = new mapboxgl.Map({
 	  container: 'map',
-	  style: 'mapbox://styles/papakhin/cketvda3s6ohy19mxh61465sm'
+	  style: 'mapbox://styles/sanisuzzaman/ckg49ouvv1crg19o0vajk9q00',
+	  center: [144.9, -37.8],
+	  zoom: 13
 	});
 	map.on('load', function() {
-	var layers = ['Completed', 'Under Construction', 'Approved', 'Applied'];
 
-	var colors = ['#6A51A4', '#9E9AC8', '#CCCAE2', '#F1EEF6'];
-	for (i = 0; i < layers.length; i++) {
-	 var layer = layers[i];
-	 var color = colors[i];
-	 var item = document.createElement('div');
-	 var key = document.createElement('span');
-	 key.className = 'legend-key';
-	 key.style.backgroundColor = color;
-	 var value = document.createElement('span');
-	 value.innerHTML = layer;
-	 item.appendChild(key);
-	 item.appendChild(value);
-	 
-	}
+ 
+});
+	map.on('click', 'live-music-venues', function(e) {
+		
+		new mapboxgl.Popup()
+		.setLngLat(e.lngLat)
+		.setHTML('<h4>Live Music Venues</h4><img src="Images/lm_01.png" style="width:100%; height:100px;"><p>Lorem ipsum dolor sit amet, Eu epicurei delicata accommodare nam. Eu propriae omnesque vel, est cu vero erat integre.</p><h2></h2>')
+		.addTo(map);
 	});
+	
+
 	map.addControl(new mapboxgl.NavigationControl());
+
+	map.addControl(
+new MapboxDirections({
+accessToken: mapboxgl.accessToken
+}),
+'top-left'
+);
+
+// Add geolocate control to the map.
+map.addControl(
+new mapboxgl.GeolocateControl({
+positionOptions: {
+enableHighAccuracy: true
+},
+trackUserLocation: true
+})
+);
